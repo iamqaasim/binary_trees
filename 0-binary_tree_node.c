647cdb10093ad488d82ae6f3c9b24cbb36f20f07
iamqaasim <iamqaasim@gmail.com>
2022-11-16 05:41:13 -0800

    My first commit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,20 +8,18 @@
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
-	if (new = NULL)
-		return(NULL);
+	if (new == NULL)
+		return (NULL);
 
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
 
-	return(new);
-
+	return (new);
 }
